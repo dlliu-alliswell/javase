f621c11a3e9a5941c7014ea8efbea46928093eeb
dlliu <412101894@qq.com>
2020-08-07 18:00:35 +0800

    io pom更改，增加main

---
+++ io/src/main/java/dl/alliswell/javase/io/Main.java
@@ -0,0 +1,24 @@
+package dl.alliswell.javase.io;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author: dlliu
+ * @date_time: 2020/8/7 13:14
+ * @description:
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        File file = new File("F:/a.txt");
+        try {
+            boolean result = file.createNewFile();
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(file);
+
+    }
+}
