73592ba45e313da73f1fab3a26850619354e69d4
dlliu <412101894@qq.com>
2020-08-25 14:22:21 +0800

    init thread

---
+++ thread/src/main/java/dl/alliswell/javase/thread/Main.java
@@ -0,0 +1,12 @@
+package dl.alliswell.javase.thread;
+
+/**
+ * @author: dlliu
+ * @date_time: 2020/8/25 9:44
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello thread.");
+    }
+}
