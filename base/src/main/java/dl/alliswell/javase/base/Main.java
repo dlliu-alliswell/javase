ed7b9f31d5de7cc8422716fa4bf2bad342e1b1ae
dlliu <412101894@qq.com>
2020-10-12 09:06:51 +0800

    ...

--- base/src/main/java/dl/alliswell/javase/base/Main.java
+++ base/src/main/java/dl/alliswell/javase/base/Main.java
@@ -1,9 +1,18 @@
 package dl.alliswell.javase.base;
 
+import java.util.List;
+
 /**
  * @author: dlliu
  * @date_time: 2020/8/13 9:45
  * @description:
  */
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = null;
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("end");
+    }
 }
